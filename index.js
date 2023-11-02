3cda1a2f37199194088bfc0e25975b202e82cc28
rhiaji <mrdavemariano@gmail.com>
2023-11-03 02:24:54 +0800

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
+const express = require("express")
+const app = express()
+app.use(express.json())
 
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes")
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"))
 
-app.use("/", userRoutes);
+app.use("/", userRoutes)
 
 // Start the server
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5002
 
 const server = app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+  console.log(`Server is listening on port localhost:${port}`)
+})
 
 // Signal that the server is ready
 server.on("listening", () => {
-  console.log("Server is fully initialized and running.");
-});
+  console.log("Server is fully initialized and running.")
+})
